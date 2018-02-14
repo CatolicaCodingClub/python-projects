62b8b9afd8daea6a044f71af4fd7c34143b881ff
perespenschug <33373770+perespenschug@users.noreply.github.com>
2018-02-14 18:09:41 +0000

    Add files via upload

---
+++ Kick off Projects/Random Numbers/Group9_guess_a_number/guess_a_number.py
@@ -0,0 +1,20 @@
+#Guess a number game
+# x is the variable of the number that has to be guessed
+# y is the variable for the number hat you guess
+# s is the variable that counts your tries
+
+import random
+x = random.randint(1,20)
+s = 0
+
+while s < 5:
+    y = int(input("Guess a number between 1 and 20:"))
+    if y == x:
+        print("Congratulations, you guessed right!")
+        break
+    if y > x:\
+        print("The number you guessed is too high")
+    else:
+        print("The number you guessed is too low")
+    s = s + 1
+    print("You have " + str(5-s) + " tries left")
